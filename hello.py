d95e092010795f7b9ca419f41b131a138efacc30
r0hitshankar <36992385+r0hitshankar@users.noreply.github.com>
2021-05-22 22:17:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello Python World from Git Rohit")
+import openpyxl
+from openpyxl.styles import Font, Alignment
+
+#Make adjustment to summary sheet and move that to start
+book = openpyxl.load_workbook("URLForMonitors.xlsx")
+#book.active = book.sheetnames.index('Summary')
+sheet = book.active
+
+import datetime
+today = datetime.datetime.now()
+print("Monitor Python file")
+print(today.strftime("%d-%b %H Hr"))
+book.save("URLForMonitors.xlsx")  
